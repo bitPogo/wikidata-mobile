3e34be246e0b9f218c716627d22889d7bac62cc5
Matthias Geisler <matthias.geisler@data4life.care>
2022-01-10 05:13:19 +0100

    Model WikibaseContract

--- mw-client/src/commonMain/kotlin/tech/antibytes/mediawiki/wikibase/WikibaseContract.kt
+++ mw-client/src/commonMain/kotlin/tech/antibytes/mediawiki/wikibase/WikibaseContract.kt
@@ -7,10 +7,19 @@
 package tech.antibytes.mediawiki.wikibase
 
 import tech.antibytes.mediawiki.EntityId
+import tech.antibytes.mediawiki.wikibase.model.Entity
 import tech.antibytes.mediawiki.wikibase.model.EntityResponse
 
 internal interface WikibaseContract {
     interface ApiService {
         fun fetchEntity(ids: Set<EntityId>): EntityResponse
+    }
+
+    interface Repository {
+        fun fetchEntity(ids: Set<EntityId>): Set<Entity>
+    }
+
+    interface Service {
+        fun fetchEntity(ids: Set<EntityId>): Set<Entity>
     }
 }
