4e7ff78bb7c85b027722a6c232163e4de8648fd0
Matthias Geisler <matthias.geisler@data4life.care>
2022-01-11 12:06:15 +0100

    Add Setup for E2E Tests

---
+++ mw-client-e2e/build.gradle.kts
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2022 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+import tech.antibytes.gradle.dependency.Dependency
+import tech.antibytes.gradle.wikidata.dependency.Dependency as LocalDependency
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+
+    // Android
+    id("com.android.library")
+
+    id("tech.antibytes.gradle.configuration")
+}
+
+kotlin {
+    android()
+
+    jvm()
+
+    sourceSets {
+        all {
+            languageSettings.apply {
+                optIn("kotlinx.coroutines.ExperimentalCoroutinesApi")
+                optIn("kotlinx.serialization.InternalSerializationApi")
+                optIn("kotlinx.serialization.ExperimentalSerializationApi")
+                optIn("kotlinx.coroutines.DelicateCoroutinesApi")
+            }
+        }
+
+        val commonMain by getting {
+            dependencies {
+                implementation(Dependency.multiplatform.kotlin.common)
+                implementation(Dependency.multiplatform.koin.core) {
+                    exclude(
+                        "org.jetbrains.kotlin",
+                        "kotlin-stdlib-jdk8"
+                    )
+                }
+
+                implementation(Dependency.multiplatform.coroutines.common)
+                implementation(Dependency.multiplatform.ktor.common.core)
+                implementation(Dependency.multiplatform.ktor.common.serialization)
+                implementation((Dependency.multiplatform.ktor.logger))
+
+                implementation(Dependency.multiplatform.serialization.common)
+                implementation(Dependency.multiplatform.dateTime)
+
+                implementation(Dependency.multiplatform.stately.freeze)
+            }
+        }
+        val commonTest by getting {
+            kotlin.srcDir("src-gen/commonTest/kotlin")
+
+            dependencies {
+                implementation(Dependency.multiplatform.test.common)
+                implementation(Dependency.multiplatform.test.annotations)
+
+                implementation(LocalDependency.antibytes.test.core)
+                implementation(LocalDependency.antibytes.test.fixture)
+                implementation(LocalDependency.antibytes.test.coroutine)
+                implementation(LocalDependency.antibytes.test.ktor)
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+               implementation(Dependency.multiplatform.kotlin.android)
+                implementation(Dependency.multiplatform.coroutines.android)
+                implementation(Dependency.multiplatform.ktor.android.client)
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                dependsOn(commonTest)
+
+                implementation(Dependency.multiplatform.test.jvm)
+                implementation(Dependency.multiplatform.test.junit)
+                implementation(Dependency.androidTest.robolectric)
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(Dependency.multiplatform.kotlin.jdk8)
+                implementation(Dependency.multiplatform.coroutines.common)
+                implementation(Dependency.multiplatform.ktor.jvm.core)
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                dependsOn(commonTest)
+
+                implementation(Dependency.multiplatform.test.jvm)
+                implementation(Dependency.multiplatform.test.junit)
+            }
+        }
+    }
+}
+
+tasks.withType(Test::class.java) {
+    testLogging {
+        events(FAILED)
+    }
+}
+
+val templatesPath = "${projectDir}/src/commonTest/resources/template"
+val configPath = "${projectDir}/src-gen/commonTest/kotlin/tech/antibytes/mediawiki/test/e2e/config"
+
+val provideTestConfig: Task by tasks.creating {
+    doFirst {
+        val templates = File(templatesPath)
+        val configs = File(configPath)
+
+        val config = File(templates, "TestConfig.tmpl")
+            .readText()
+            .replace("PROJECT_DIR", projectDir.toPath().toAbsolutePath().toString())
+
+        if (!configs.exists()) {
+            if(!configs.mkdir()) {
+                System.err.println("The script not able to create the config directory")
+            }
+        }
+        File(configPath, "TestConfig.kt").writeText(config)
+    }
+}
+
+tasks.withType(org.jetbrains.kotlin.gradle.dsl.KotlinCompile::class.java) {
+    if (this.name.contains("Test")) {
+        this.dependsOn(provideTestConfig)
+    }
+}
