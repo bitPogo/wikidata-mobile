95210c9161e7c916fbf677ff3644c996c1ff2368
Matthias Geisler <matthias.geisler@data4life.care>
2022-01-19 06:59:37 +0100

    Add UserStore Setup

--- user-store/build.gradle.kts
+++ user-store/build.gradle.kts
@@ -16,12 +16,6 @@ plugins {
 
     id("tech.antibytes.gradle.configuration")
     id("tech.antibytes.gradle.coverage")
-
-    // SqlDelight
-    id("com.squareup.sqldelight")
-
-    // Serialization
-    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 kotlin {
@@ -33,8 +27,6 @@ kotlin {
         all {
             languageSettings.apply {
                 optIn("kotlinx.coroutines.ExperimentalCoroutinesApi")
-                optIn("kotlinx.serialization.InternalSerializationApi")
-                optIn("kotlinx.serialization.ExperimentalSerializationApi")
                 optIn("kotlinx.coroutines.DelicateCoroutinesApi")
             }
         }
@@ -57,15 +49,11 @@ kotlin {
 
                 implementation(Dependency.multiplatform.stately.freeze)
 
-                implementation(LocalDependency.sqldelight.coroutines)
-
                 implementation(project(":mw-client"))
                 implementation(project(":utils-coroutine"))
             }
         }
         val commonTest by getting {
-            kotlin.srcDir("src-gen/commonTest/kotlin")
-
             dependencies {
                 implementation(Dependency.multiplatform.test.common)
                 implementation(Dependency.multiplatform.test.annotations)
@@ -80,7 +68,6 @@ kotlin {
             dependencies {
                implementation(Dependency.multiplatform.kotlin.android)
                 implementation(Dependency.multiplatform.coroutines.android)
-                implementation(LocalDependency.sqldelight.android)
             }
         }
         val androidTest by getting {
@@ -99,7 +86,6 @@ kotlin {
             dependencies {
                 implementation(Dependency.multiplatform.kotlin.jdk8)
                 implementation(Dependency.multiplatform.coroutines.common)
-                implementation(LocalDependency.sqldelight.jvm)
             }
         }
         val jvmTest by getting {
@@ -124,40 +110,5 @@ android {
 tasks.withType(Test::class.java) {
     testLogging {
         events(FAILED)
-    }
-}
-
-val templatesPath = "${projectDir}/src/commonTest/resources/template"
-val configPath = "${projectDir}/src-gen/commonTest/kotlin/tech/antibytes/wikibase/store/entity/test/config"
-
-val provideTestConfig: Task by tasks.creating {
-    doFirst {
-        val templates = File(templatesPath)
-        val configs = File(configPath)
-
-        val config = File(templates, "TestConfig.tmpl")
-            .readText()
-            .replace("PROJECT_DIR", projectDir.toPath().toAbsolutePath().toString())
-
-        if (!configs.exists()) {
-            if(!configs.mkdir()) {
-                System.err.println("The script not able to create the config directory")
-            }
-        }
-        File(configPath, "TestConfig.kt").writeText(config)
-    }
-}
-
-tasks.withType(org.jetbrains.kotlin.gradle.dsl.KotlinCompile::class.java) {
-    if (this.name.contains("Test")) {
-        this.dependsOn(provideTestConfig)
-    }
-}
-
-sqldelight {
-    database("WikibaseDataBase") {
-        packageName = "tech.antibytes.wikibase.store.database.entity"
-        sourceFolders = listOf("database")
-        verifyMigrations = true
     }
 }
