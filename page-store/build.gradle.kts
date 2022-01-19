98a4eccc6c2ebb9f2984a4b6db495372ad48cc90
Matthias Geisler <matthias.geisler@data4life.care>
2022-01-19 11:58:33 +0100

    Add page store setup

---
+++ page-store/build.gradle.kts
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2022 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+import tech.antibytes.gradle.dependency.Dependency
+import tech.antibytes.gradle.wikidata.dependency.Dependency as LocalDependency
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+
+    // Android
+    id("com.android.library")
+
+    id("tech.antibytes.gradle.configuration")
+    id("tech.antibytes.gradle.coverage")
+}
+
+kotlin {
+    android()
+
+    jvm()
+
+    sourceSets {
+        all {
+            languageSettings.apply {
+                optIn("kotlinx.coroutines.ExperimentalCoroutinesApi")
+                optIn("kotlinx.coroutines.DelicateCoroutinesApi")
+            }
+        }
+
+        val commonMain by getting {
+            dependencies {
+                implementation(Dependency.multiplatform.kotlin.common)
+                implementation(Dependency.multiplatform.koin.core) {
+                    exclude(
+                        "org.jetbrains.kotlin",
+                        "kotlin-stdlib-jdk8"
+                    )
+                }
+
+                implementation(Dependency.multiplatform.coroutines.common)
+
+                implementation(Dependency.multiplatform.serialization.common)
+                implementation(Dependency.multiplatform.serialization.json)
+                implementation(Dependency.multiplatform.dateTime)
+
+                implementation(Dependency.multiplatform.stately.freeze)
+
+                implementation(project(":mw-client"))
+                implementation(project(":utils-coroutine"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(Dependency.multiplatform.test.common)
+                implementation(Dependency.multiplatform.test.annotations)
+
+                implementation(LocalDependency.antibytes.test.core)
+                implementation(LocalDependency.antibytes.test.fixture)
+                implementation(LocalDependency.antibytes.test.coroutine)
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+               implementation(Dependency.multiplatform.kotlin.android)
+                implementation(Dependency.multiplatform.coroutines.android)
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                dependsOn(commonTest)
+
+                implementation(Dependency.multiplatform.test.jvm)
+                implementation(Dependency.multiplatform.test.junit)
+                implementation(Dependency.android.test.ktx)
+                implementation(Dependency.android.test.robolectric)
+                implementation(Dependency.android.test.junit)
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(Dependency.multiplatform.kotlin.jdk8)
+                implementation(Dependency.multiplatform.coroutines.common)
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                dependsOn(commonTest)
+
+                implementation(Dependency.multiplatform.test.jvm)
+                implementation(Dependency.multiplatform.test.junit)
+            }
+        }
+    }
+}
+
+android {
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
+
+tasks.withType(Test::class.java) {
+    testLogging {
+        events(FAILED)
+    }
+}
