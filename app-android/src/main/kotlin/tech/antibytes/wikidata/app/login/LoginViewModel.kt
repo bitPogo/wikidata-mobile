759cb3f90c7c0d32de52c3ec9d57063666db8a26
Matthias Geisler <matthias.geisler@data4life.care>
2022-01-27 18:28:36 +0100

    Add LoginViewmodel

--- app-android/src/main/kotlin/tech/antibytes/wikidata/app/login/LoginViewModel.kt
+++ app-android/src/main/kotlin/tech/antibytes/wikidata/app/login/LoginViewModel.kt
@@ -10,14 +10,16 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import tech.antibytes.util.coroutine.result.ResultContract
 import tech.antibytes.wikibase.store.user.UserStoreContract
+import tech.antibytes.wikibase.store.user.lang.UserStoreError
 
 class LoginViewModel(
-    private val userStore: UserStoreContract.UserStore
+    private val userStore: UserStoreContract.UserStore,
 ) : LoginContract.LoginViewModel, ViewModel() {
-    private val _isLoggedIn = MutableStateFlow(false)
+    private val _isLoggedIn = MutableStateFlow<LoginContract.LoginState>(LoginContract.LoginState.LoggedOut)
 
-    override val isLoggedIn: StateFlow<Boolean> = _isLoggedIn
+    override val isLoggedIn: StateFlow<LoginContract.LoginState> = _isLoggedIn
 
     private val _username = MutableStateFlow("")
 
@@ -28,10 +30,34 @@ class LoginViewModel(
     override val password: StateFlow<String> = _password
 
     init {
-        userStore.isAuthenticated.subscribeWithSuspendingFunction { result ->
-            if (!result.isError()) {
-                _isLoggedIn.update { result.unwrap() }
-            }
+        userStore.isAuthenticated.subscribe { result ->
+            subscription(result)
+        }
+    }
+
+    private fun subscription(result: ResultContract<Boolean, Exception>) {
+        val newState = if (!result.isError()) {
+            mapSuccess(result)
+        } else {
+            mapError(result)
+        }
+
+        _isLoggedIn.update { newState }
+    }
+
+    private fun mapSuccess(result: ResultContract<Boolean, Exception>): LoginContract.LoginState {
+        return if (result.unwrap()) {
+            LoginContract.LoginState.LoggedIn
+        } else {
+            LoginContract.LoginState.LoggedOut
+        }
+    }
+
+    private fun mapError(result: ResultContract<Boolean, Exception>): LoginContract.LoginState {
+        return if (result.error is UserStoreError.InitialState) {
+            LoginContract.LoginState.LoggedOut
+        } else {
+            LoginContract.LoginState.AuthenticationError(LoginContract.Reason.WRONG_USERNAME_OR_PASSWORD)
         }
     }
 
@@ -48,5 +74,7 @@ class LoginViewModel(
             _username.value,
             _password.value
         )
+
+        setPassword("")
     }
 }
