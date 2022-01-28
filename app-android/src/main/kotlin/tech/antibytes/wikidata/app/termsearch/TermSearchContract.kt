ebbcaa024ae01025b55474fec2256dd3a2286534
Matthias Geisler <matthias.geisler@data4life.care>
2022-01-28 04:55:55 +0100

    Add TermSearchViewModel

---
+++ app-android/src/main/kotlin/tech/antibytes/wikidata/app/termsearch/TermSearchContract.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2022 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package tech.antibytes.wikidata.app.termsearch
+
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import tech.antibytes.wikibase.store.page.domain.model.PageModelContract
+
+interface TermSearchContract {
+    interface TermSearchViewModel {
+        val result: SharedFlow<List<PageModelContract.SearchEntry>>
+        val query: StateFlow<String>
+
+        fun setQuery(query: String)
+        fun search()
+    }
+}
